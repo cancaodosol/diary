77e6c2758e790c5c59f31438c2e7d5f5e0ffa89d
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-06-15 02:18:14 +0900

    引用部分の装飾追加

--- app/src/Entity/Diary.php
+++ app/src/Entity/Diary.php
@@ -150,6 +150,10 @@ class Diary
         $textHtml = str_replace('[code]', '<div class="code-text" style="font-size:0.8rem;"><script type="text/plain" class="language-php line-numbers">', $textHtml);
         $textHtml = str_replace('[/code]', '</script></div>', $textHtml);
 
+        # [引用][/引用]の置換
+        $textHtml = str_replace('[引用]', '<div class="box"><p>', $textHtml);
+        $textHtml = str_replace('[/引用]', '</p></div>', $textHtml);
+
         return $textHtml;
     }
     public function setText(string $text): self
